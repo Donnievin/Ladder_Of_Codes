5fa14405a66b3c92ae77082c8d1b4b39526ba8ee
Donnievin <51129671+Donnievin@users.noreply.github.com>
2019-07-08 13:57:02 -0400

    Update StarsVel_Multiple.py

--- StarsVel_Multiple.py
+++ StarsVel_Multiple.py
@@ -2,9 +2,9 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 import pynbody
-import pylab
+#import pylab
 import numpy as np
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 import astropy.units as u
 import readcol
 
