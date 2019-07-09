dbda62f303aead4e4d337aa55aeaf718712302e1
Donnievin <51129671+Donnievin@users.noreply.github.com>
2019-07-09 16:25:27 -0400

    Update StarsVel_Multiple.py

--- StarsVel_Multiple.py
+++ StarsVel_Multiple.py
@@ -5,59 +5,54 @@ import pynbody
 import numpy as np
 import readcol
 
-'''
-Steps to creating  for loop
-1. Create a file/array/list you want to cycle through
-2. "For number (x) in [range]: if "
-3. Tell it what you want it to do for all the x's in this file
-4. print x
-'''
-
-#Now I need a code that will load the snapshots(s will stand for simulation)
-#s = pynbody.load("cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096.5.std")
-#"/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/"
-
+#Now I need a code that will load the snapshots(s will stand for )
+Path = "/media/jillian//cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/"
 files = readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/files.list')
-all_files = files[0:]
-'''
-#This is a function to find the blackholes
+all_files = files[:,0]
+
+#Tell where BH is Function
 def findBH(s):
     BH = s.stars[pynbody.filt.LowPass('tform', 0.0)]
     return BH
-BH = findBH(s)
-print(BH)
-'''
-'''
-#We used this function to calculate the velocity of the stars
 
-#Here I will make a another loop which will solve for vel around BH
-radius = 0.5 #KPC
-sphere= pynbody.filt.Sphere(radius, cen=  pynbody.analysis.angmom.faceon(all_files))
-#This code tells us how many stars are in this section
-num_of_stars = all_files.stars[0:]
-in_sphere = num_of_stars[sphere]
-#This is how many stars there are
-total_stars = len(in_sphere)
-#This find their velocity
-velocity = in_sphere['vel']
-#Now we need to find the velocity of these stars in x,y,z
-x = np.array([vel[0] for vel in velocity])
-y = np.array([vel[1] for vel in velocity])
-z = np.array([vel[2] for vel in velocity])
-#Now we can find the average of these by dividing by the total
-vel_answer = np.sqrt((x)**2 + (y)**2 + (z)**2)
-#Now divide by total number of stars
-velocity = vel_answer.sum() / total_stars
-'''
+#Give BH pos variable
 
-#Below is a for loop outline
-'''
-for num in range(5):
-    print(num)
-'''
-for i in all_files :
+
+for i in all_files:
+    s = pynbody.load(Path + i)
+    s.physical_units()
+    BH = findBH(s)
+    #Don't forget to center the galaxy with this
+    pynbody.analysis.angmom.faceon(s)
+    #printing the position by itself will give us an array of i,j,k
+    #Instead use the function we made before
+    BH_pos = BH['pos']
+    BHx = BH_pos[:,0]
+    BHy = BH_pos[:,1]
+    BHz = BH_pos[:,2]
+    BH_answer = np.sqrt((BHx)**2 + (BHy)**2 + (BHz)**2)
+    print(BH_answer)
     #Center it
-    pynbody.analysis.angmom.faceon(all_files)
-    stars_vel = velocity[i]
-    print 'The stars around the black hole are moving at:', stars_vel
+    pynbody.analysis.angmom.faceon(s)
+    #Here I will make a another loop which will solve for vel around BH
+    radius = 0.5 #KPC
+    len_three = np.array([BHx[0], BHy[0], BHz[0]])
+    print(len_three)
+    sphere= pynbody.filt.Sphere(radius, cen= (len_three))
+    #This code tells us how many stars are in this section
+    num_of_stars = s.stars[0:]
+    in_sphere = num_of_stars[sphere]
+    #This is how many stars there are
+    total_stars = len(in_sphere)
+    #This find their velocity
+    velocity = in_sphere['vel']
+    #Now we need to find the velocity of these stars in x,y,z
+    x = np.array([vel[0] for vel in velocity])
+    y = np.array([vel[1] for vel in velocity])
+    z = np.array([vel[2] for vel in velocity])
+    #Now we can find the average of these by dividing by the total
+    vel_answer = np.sqrt((x)**2 + (y)**2 + (z)**2)
+    #Now divide by total number of stars
+    velocity = vel_answer.sum() / total_stars
 
+    print 'The stars around the black hole in this snapshot are moving at:', velocity
