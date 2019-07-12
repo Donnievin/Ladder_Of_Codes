7b237cb803b85bb129b42661832829918f0f250a
Donnievin <51129671+Donnievin@users.noreply.github.com>
2019-07-11 21:57:37 -0400

    Update Reading_Mult_Snaps.py

--- Reading_Mult_Snaps.py
+++ Reading_Mult_Snaps.py
@@ -2,44 +2,29 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 import pynbody
-import pylab
 import numpy as np
-import matplotlib.pylab as plt
-import astropy.units as u
+import readcol
+import matplotlib.pyplot as plt
 
-'''
-Steps to creating  for loop
-1. Create a file/array/list you want to cycle through
-2. "For number (x) in [range]: if "
-3. Tell it what you want it to do for all the x's in this file
-4. print x
-'''
+#Now I need a code that will load the snapshots(s will stand for )
+Path = "/media/jillian//cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/"
+files = readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/files.list')
+all_files = files[:,0]
 
-#Now I need a code that will load the simulation (s will stand for simulation)
-s = pynbody.load("cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096.5.std")
-
-#The following code will change the units to make it more appealing
-s.physical_units()
-#This is a function to find the blackholes
+#Tell where BH is Function
 def findBH(s):
     BH = s.stars[pynbody.filt.LowPass('tform', 0.0)]
     return BH
-BH = findBH(s)
+
+#Give BH pos variable
 
 
-#Below is a for loop outline
-'''
-for num in range(5):
-    print(num)
-'''
-
-'''
-#Here is a for loop that will find each black hole in the entire simulation
-#It will print their positions
-for x in s:
+for i in all_files:
+    s = pynbody.load(Path + i)
+    s.physical_units()
+    BH = findBH(s)
     #Don't forget to center the galaxy with this
     pynbody.analysis.angmom.faceon(s)
-
     #printing the position by itself will give us an array of i,j,k
     #Instead use the function we made before
     BH_pos = BH['pos']
@@ -47,14 +32,14 @@ for x in s:
     BHy = BH_pos[:,1]
     BHz = BH_pos[:,2]
     BH_answer = np.sqrt((BHx)**2 + (BHy)**2 + (BHz)**2)
-    print "I found the black hole and it is at:" , BH_answer
-'''
-    
-for i in s:
-
+    print(BH_answer)
+    #Center it
+    pynbody.analysis.angmom.faceon(s)
     #Here I will make a another loop which will solve for vel around BH
     radius = 0.5 #KPC
-    sphere= pynbody.filt.Sphere(radius, cen= (-634.00464133,1258.07020815, 29.86851614))
+    len_three = np.array([BHx[0], BHy[0], BHz[0]])
+    #print(len_three)
+    sphere= pynbody.filt.Sphere(radius, cen= (len_three))
     #This code tells us how many stars are in this section
     num_of_stars = s.stars[0:]
     in_sphere = num_of_stars[sphere]
@@ -68,16 +53,15 @@ for i in s:
     z = np.array([vel[2] for vel in velocity])
     #Now we can find the average of these by dividing by the total
     vel_answer = np.sqrt((x)**2 + (y)**2 + (z)**2)
-
     #Now divide by total number of stars
     velocity = vel_answer.sum() / total_stars
-    answer = velocity
-    print 'The stars around the black hole are moving at:', answer
 
-'''
-    
-    BH_vel = BH[
-    print(['vel'])
-    print(I found youre blackhole)
-'''
-    
+    print 'The stars around the black hole in this snapshot are moving at:', velocity
+
+    pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo11Faceon.png')
+    # center it
+    pynbody.analysis.angmom.faceon(s)
+
+    # create an image using  the default bands (i, v, u)
+    pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo11edgeon.png')
+    plt.show() 
