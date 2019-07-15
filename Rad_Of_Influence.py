0224adf63c77823d30ed7a76319c455478cee2a7
Donnievin <51129671+Donnievin@users.noreply.github.com>
2019-07-15 10:20:13 -0400

    Create Rad_Of_Influence.py

---
+++ Rad_Of_Influence.py
@@ -0,0 +1,8 @@
+#import all the good stuff!
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+import pynbody
+import pylab
+import numpy as np
+import matplotlib.pylab as plt
+import astropy.units as u
